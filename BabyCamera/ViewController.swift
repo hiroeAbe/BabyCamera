916012684f2cd0ab45717299fb2ad33f9172a0a2
hiroeAbe <g14901ah@gm.tsuda.ac.jp>
2016-11-15 15:13:10 +0900

    Initial Commit

---
+++ BabyCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BabyCamera
+//
+//  Created by 阿部弘枝 on 2016/11/15.
+//  Copyright © 2016年 阿部弘枝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
